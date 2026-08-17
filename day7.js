1e1bba426701c6b0744d19319388abd4c23bdafc
Harshit83183 <harshittiwari964841@gmail.com>
2026-08-17 09:30:50 +0530

    commit

---
+++ day7.js
@@ -0,0 +1,9 @@
+//create your own server using HTTp module
+const http=require('http');
+const server=http.createServer((req, res)=>{
+
+})
+server.listen(8000, ()=>{
+    console.log("server is running on 8000 port")
+})
+//hello
